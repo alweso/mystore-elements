57b0734864b008eeb61bf479097e4c7bc253b630
alweso <alweso@gmail.com>
2021-03-12 16:44:37 +0100

    error but should work

--- widgets/meta-store-product-grid-widget.php
+++ widgets/meta-store-product-grid-widget.php
@@ -37,17 +37,29 @@ class My_Store_Product_Grid_Widget extends \Elementor\Widget_Base {
     $this->add_control(
       'pick_by', [
         'label' => __('Pick Products By', 'meta-store-elements'),
-        'type' => \Elementor\Controls_Manager::SELECT2,
+        'type' => \Elementor\Controls_Manager::SELECT,
         'default' => 0,
-        'multiple' => true,
         'options'   => [
-          'all'      =>esc_html__( 'All', 'menheer-plugin' ),
-          'total_sales'      =>esc_html__( 'Bestsellers', 'menheer-plugin' ),
-          ''      =>esc_html__( 'Top Rated', 'menheer-plugin' ),
-          ''      =>esc_html__( 'On Sale', 'menheer-plugin' ),
+          'latest'      =>esc_html__( 'Latest', 'menheer-plugin' ),
+          'bestsellers'      =>esc_html__( 'Bestsellers', 'menheer-plugin' ),
+          'toprated'      =>esc_html__( 'Top Rated', 'menheer-plugin' ),
+          'onsale'      =>esc_html__( 'On Sale', 'menheer-plugin' ),
+          'featured'      =>esc_html__( 'Featured', 'menheer-plugin' ),
         ],
       ]
     );
+
+    $this->add_control(
+      'number_of_products',
+      [
+        'label' => __( 'Number of products', 'menheer-plugin' ),
+        'type' => Controls_Manager::NUMBER,
+        'default' => __( 4, 'menheer-plugin' ),
+        'min' => 1,
+        'step' => 1,
+      ]
+    );
+
 
     $this->end_controls_section();
 
@@ -139,83 +151,85 @@ class My_Store_Product_Grid_Widget extends \Elementor\Widget_Base {
 
   /** Render Layout */
   protected function render() {
-    // $args = array(
-    // 'post_type' => 'product',
-    // // 'meta_key' => '_featured',
-    // // 'meta_value' => 'yes',
-    // 'orderby' => 'meta_value_num',
-    // 'posts_per_page' => 12,
-    // // 'meta_query' => array(
-    // //     array(
-    // //         'key'     => 'age',
-    // //         'value'   => array( 3, 4 ),
-    // //         'compare' => 'IN',
-    // //     ),
-    // // ),
-    // );
+    $settings = $this->get_settings_for_display();
+    $pickProductBy = $settings['pick_by'];
+    // $numberOfProducts = $settings['number_of_products'];
 
-    // Bestsellers
-    // $args = array(
-    //     'post_type' => 'product',
-    //     'meta_key' => 'total_sales',
-    //     'orderby' => 'meta_value_num',
-    //     'posts_per_page' => 12,
-    // );
 
-//     // on sale - show all sale products, even those after or before sale periods - to do
-//
-        // $args = array(
-        //         'post_type' => 'product',
-        //         // 'meta_key' => '_sale_price',
-        //         // 'orderby' => 'rating',
-        //          'post__in' => wc_get_product_ids_on_sale(),
-        //         'order' => 'DESC',
-        //         // 'meta_query' => array(
-        //         //   array(
-        //         //             'key' => '_sale_price',
-        //         //             'value' => '',
-        //         //             'compare' => '!='
-        //         //         ),
-        //         //         ),
-        //         );
+   switch ($pickProductBy) {
+      case 'latest':
+      $args = array(
+        'post_type' => 'product',
+        // 'orderby' => 'rating',
+        'order' => 'DESC',
+        'posts_per_page' => 12,
+      );
+      break;
 
-                // featured working!
-              //
-                    $args = array(
-                            'post_type' => 'product',
-                            // 'meta_key' => '_featured',
-                            'orderby' => 'rating',
-                            'order' => 'DESC',
-                            'post__in' => wc_get_featured_product_ids(),
-                            // 'tax_query' => array(
-                            //     'taxonomy' => 'product_visibility',
-                            //     'field'    => 'name',
-                            //     'terms'    => 'featured',
-                            //     'operator' => 'IN', // or 'NOT IN' to exclude feature products
-                            // ),
-                            );
+      case 'toprated':
+      $args = array(
+        'post_type' => 'product',
+        // 'meta_key' => 'total_sales',
+        // 'orderby' => 'ratings',
+        'orderby'   => 'meta_value_num',
+        'meta_key'  => '_wc_average_rating',
+        'order' => 'DESC',
+        'posts_per_page' => 12,
+      );
+      break;
+
+      case 'featured':
+      $args = array(
+        'post_type' => 'product',
+        // 'orderby' => 'rating',
+        'order' => 'DESC',
+        'post__in' => wc_get_featured_product_ids(),
+      );
+      break;
+
+      case 'onsale':
+      $args = array(
+        'post_type' => 'product',
+        // 'orderby' => 'rating',
+        'post__in' => wc_get_product_ids_on_sale(),
+        'order' => 'DESC',
+      );
+      break;
+
+      case 'bestsellers':
+      $args = array(
+        'post_type' => 'product',
+        'meta_key' => 'total_sales',
+        'orderby' => 'meta_value_num',
+        'posts_per_page' => 12,
+      );
+      break;
+
+    }
+
+
+
+
 
     $loop = new WP_Query( $args );?>
-    <div style="display:grid;grid-template-columns:1fr 1fr 1fr 1fr">
+    <div style="display:grid;grid-template-columns:1fr 1fr 1fr 1fr;grid-column-gap:15px;grid-row-gap:15px;">
       <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-                  <?php  $product = get_product($loop->post); ?>
+        <?php  $product = get_product($loop->post); ?>
         <div>
           <!-- <?php echo '<pre>' . var_export($product, true) . '</pre>'; ?> -->
-          <?php
-$date = new DateTime();
-echo $date->format('Y-m-d H:i:sP');
-?>
+
           <hr></hr>
           <!-- <h1><?php echo $product->date_on_sale_to ?></h1> -->
           <!-- <h1><?php echo   $product->get_featured(); ?></h1> -->
 
-          <?php echo woocommerce_get_product_thumbnail('woocommerce_full_size'); ?>
+          <?php echo woocommerce_get_product_thumbnail('woocommerce_thumbnail'); ?>
           <a href="<?php echo get_permalink( $product->ID ) ?>">
             <?php the_title(); ?>
-            <p>average rating<?php echo $product->get_average_rating();?></p>
-            <p>regular price<?php echo $product->get_regular_price(); ?></p>
-            <p>sale price<?php echo $product->get_sale_price(); ?></p>
           </a>
+          <p>average rating<?php echo $product->get_average_rating();?></p>
+          <p>regular price<?php echo $product->get_regular_price(); ?></p>
+          <p>sale price<?php echo $product->get_sale_price(); ?></p>
+          <a href="<?php echo $product->add_to_cart_url() ?>" value="<?php echo esc_attr( $product->get_id() ); ?>" class="ajax_add_to_cart add_to_cart_button" data-product_id="<?php echo get_the_ID(); ?>" data-product_sku="<?php echo esc_attr($sku) ?>"> Add to Cart </a>
         </div>
       <?php endwhile;?>
     </div>
